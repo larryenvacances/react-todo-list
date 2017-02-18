566c8e7f739097208323a31afdfd11e279ef934c
Laurent Huot <huot.laurent11@gmail.com>
2017-02-18 18:52:10 -0500

    L'app est maintenant en react avec App.js comme root

--- client/client.js
+++ client/client.js
@@ -1,1 +1,10 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from '../components/App.js'
 
+render(
+    // define the encompassing component
+    // DOM element dans lequel on veut sacrer le component
+    <App/>,
+    document.getElementById('app')
+)
--- components/App.js
+++ components/App.js
@@ -1,1 +1,9 @@
+import React, { Component } from 'react'
 
+class App extends Component {
+    render() {
+        return <div>Ceci est maintenant une app React</div>
+    }
+}
+
+export default App
